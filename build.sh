d5c86c4df178422e7799dce645717f5c52557c03
Salvatore Iovene <salvatore.iovene@intel.com>
2015-12-17 13:02:39 +0200

    Freeze to Crosswalk 18.46.455.0

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 ARCH="x86"
 EXTENSION_LIB="libcrosswalk-oic.so"
-EXTENSION_DEST="org.crosswalk.demo.oic/prj/android/libs/$ARCH"
+PACKAGE="org.crosswalk.demo.oic"
+EXTENSION_DEST="$PACKAGE/prj/android/libs/$ARCH"
 
-if [ ! -d org.crosswalk.demo.oic ]; then
-    crosswalk-app create org.crosswalk.demo.oic --android-crosswalk=canary
-fi
-
-rm *.apk 1>&2 >/dev/null
+rm -f *.apk 
 (cd crosswalk-oic-extension; ./build.sh)
 mkdir -p $EXTENSION_DEST
 cp crosswalk-oic-extension/out/$ARCH/$EXTENSION_LIB $EXTENSION_DEST
-crosswalk-app build org.crosswalk.demo.oic
+crosswalk-app build $PACKAGE
